41cb82f8fd4f84131e45bd7e76302d842aafef63
Patrick James <pjjames@mymail.mines.edu>
2021-06-02 21:13:33 -0600

    small addition of last in list

--- LinkedLists.py
+++ LinkedLists.py
@@ -17,9 +17,17 @@ class Node:
             print(self.data, end =" ")
             self.next.print()
 
+    def printLast(self):
+        if self.next is None:
+            print("\n Last node data: ", self.data)
+            return
+        else:
+            self.next.printLast()
+
 
 list = Node(0)
 list.append(1)
 list.append(2)
 list.append(3)
 list.print()
+list.printLast()
